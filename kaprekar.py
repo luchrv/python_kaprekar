4d7eb04b5fd1fc36407d18394fe3b5ffa6488359
Evers Rivero <evers.rivero@gmail.com>
2019-11-14 12:04:45 -0500

    Agregadas las excepciones.

--- kaprekar.py
+++ kaprekar.py
@@ -50,9 +50,15 @@ print("")
 # Pedimos que ingresen un número de 4 dígitos
 numero = input("Introduce un número de cuatro dígitos: ")
 
+
+exclusiones = [1111, 2222, 3333, 4444, 5555, 6174, 6666, 7777, 8888, 9999]
+
 # Evaluamos que el valor introducido sea un número y que sea de cuatro dígitos
 if numero.isnumeric() and len(numero) == 4:
-    print("Procesando el número:", numero, " para llegar a la constante de Kaprekar:")
-    procesar(numero)
+    if not int(numero) in exclusiones:
+        print("Procesando el número:", numero, " para llegar a la constante de Kaprekar:")
+        procesar(numero)
+    else:
+        print("No se permiten número de cuatro dígitos iguales (Ej. 1111), ni el 6174.")
 else:
     print("No es un número de 4 dígitos.")
